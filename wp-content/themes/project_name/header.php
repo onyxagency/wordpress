a73e6105992313a29068b396163f9f3fcd06fb8f
teamjordy <teamjord@gmail.com>
2016-06-10 12:58:06 -0400

    Updating sass partials, head, and favicons

--- wp-content/themes/project_name/header.php
+++ wp-content/themes/project_name/header.php
@@ -1,10 +1,10 @@
-<?php ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title><?php wp_title(''); ?></title>
 		<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
     <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
